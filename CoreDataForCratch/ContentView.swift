68a304d3ee4532df5e6624b94e97a3f2576f51b9
hanad.ahmed <Hanadahmed1414@gmail.com>
2024-02-12 09:40:59 +0100

    Initial Commit

---
+++ CoreDataForCratch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreDataForCratch
+//
+//  Created by Hanad.Ahmed on 2024-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreDataForCratch/CoreDataForCratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataForCratchApp.swift
+//  CoreDataForCratch
+//
+//  Created by Hanad.Ahmed on 2024-02-12.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataForCratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
